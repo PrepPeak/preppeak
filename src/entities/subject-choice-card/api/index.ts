541e0bb0022fe0b0118c828afbddcdde17431a88
Yerlan <65305282+snaprick@users.noreply.github.com>
2024-04-14 18:10:09 +0000

    fix: base url & subject slash

--- src/entities/subject-choice-card/api/index.ts
+++ src/entities/subject-choice-card/api/index.ts
@@ -3,7 +3,7 @@ import { SubjectsType } from "@/entities/subject-choice-card/models";
 import { request } from "@/shared";
 
 export const getSubjects = (): Promise<AxiosResponse<SubjectsType>> => {
-  const url = "/subjects";
+  const url = "/subjects/";
 
   return request.get(url);
 };
